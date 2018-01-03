6a9faf2c09cb71fd2a11869ac2cd390bd94323e3
Pierre Moreau <pierre.morrow@free.fr>
2018-01-04 00:10:09 +0100

    clover/api: Implement CL_DEVICE_IL_VERSION_KHR

--- src/gallium/state_trackers/clover/api/device.cpp
+++ src/gallium/state_trackers/clover/api/device.cpp
@@ -333,6 +333,13 @@ clGetDeviceInfo(cl_device_id d_dev, cl_device_info param,
       buf.as_string() = dev.supported_extensions();
       break;
 
+   case CL_DEVICE_IL_VERSION_KHR:
+      if (dev.supported_extensions().find("cl_khr_il_program") == std::string::npos)
+         throw error(CL_INVALID_VALUE);
+      buf.as_string() = std::string("SPIR-V_1.0");
+
+      break;
+
    case CL_DEVICE_PLATFORM:
       buf.as_scalar<cl_platform_id>() = desc(dev.platform);
       break;
