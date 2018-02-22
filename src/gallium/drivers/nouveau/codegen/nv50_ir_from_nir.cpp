0e202a26ba3b77c986e0dca912fffe185d779d42
Karol Herbst <kherbst@redhat.com>
2018-02-22 12:00:01 +0100

    nvir/nir: allow non main functions

--- src/gallium/drivers/nouveau/codegen/nv50_ir_from_nir.cpp
+++ src/gallium/drivers/nouveau/codegen/nv50_ir_from_nir.cpp
@@ -1464,8 +1464,6 @@ Converter::parseNIR()
 bool
 Converter::visit(nir_function *function)
 {
-   // we only support emiting the main function for now
-   assert(!strcmp(function->name, "main"));
    assert(function->impl);
 
    // usually the blocks will set everything up, but main is special
