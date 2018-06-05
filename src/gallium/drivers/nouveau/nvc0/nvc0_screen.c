a447811df137ba82a196a46a6d29db1d1d18a423
Karol Herbst <kherbst@redhat.com>
2018-06-05 14:12:55 +0200

    nouveau: expose HMM

--- src/gallium/drivers/nouveau/nvc0/nvc0_screen.c
+++ src/gallium/drivers/nouveau/nvc0/nvc0_screen.c
@@ -336,6 +336,8 @@ nvc0_screen_get_param(struct pipe_screen *pscreen, enum pipe_cap param)
    case PIPE_CAP_MAX_COMBINED_HW_ATOMIC_COUNTERS:
    case PIPE_CAP_MAX_COMBINED_HW_ATOMIC_COUNTER_BUFFERS:
       return 0;
+   case PIPE_CAP_SVM_SUPPORT:
+      return 1 << 2;
 
    case PIPE_CAP_VENDOR_ID:
       return 0x10de;
